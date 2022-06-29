f8bcd5c230b302d657570e8eeb0b1e54affe7a6f
Latrevion <xiaopikaqiu@outlook.com>
2022-06-29 23:05:52 +0800

    项目结构、组件样式

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,3 +1,23 @@
-export default  {
 
+
+export default  {
+  data(){
+    return{
+     msg:'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+   onClick1(){
+     this.$message.error("错了哦，这是错误的信息")
+  } ,
+    onClick2(){
+      this.$alert('这是一段内容', '标题名称', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message.success("点了确定")
+        }
+      })
+    }
+  }
 }
+
