83c78ce1bceb6db1156645e2e8a7c3e276754eb3
Latrevion <xiaopikaqiu@outlook.com>
2022-07-04 16:43:17 +0800

    完善编辑页面功能

--- src/pages/Edit/template.vue
+++ src/pages/Edit/template.vue
@@ -2,14 +2,15 @@
   <div id="edit">
     <h1>编辑文章</h1>
     <h3>文章标题</h3>
-    <el-input v-model="title"></el-input>
-    <p class="msg">限30个字</p>
+    <el-input maxlength="50" v-model="title" placeholder="请输入文章标题"></el-input>
+    <p class="msg">限50个字</p>
     <h3>内容简介</h3>
-    <el-input type="textarea" v-model="description" :autosize="{ minRows: 2, maxRows: 6}"></el-input>
-    <p class="msg">限30个字</p>
+    <el-input  maxlength="100" type="textarea" v-model="description" :autosize="{ minRows: 1, maxRows: 6}" placeholder="请输入内容简介"></el-input>
+    <p class="msg">限100个字</p>
     <h3>文章内容</h3>
-    <el-input type="textarea" v-model="content" :autosize="{ minRows: 4, maxRows: 30}"></el-input>
-    <p class="msg">限30个字</p>
+    <p class="msg">至少200个字</p>
+    <el-input type="textarea" v-model="content" :autosize="{ minRows: 4, maxRows: 30}" placeholder="请输入文章内容"></el-input>
+    <Emoji></Emoji>
     <p>
       <label>是否展示到首页</label>
       <el-switch v-model="atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
