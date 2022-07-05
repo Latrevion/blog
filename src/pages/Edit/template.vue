d34a3985b370189d75be63b160737c015b1bbf5c
Latrevion <xiaopikaqiu@outlook.com>
2022-07-05 20:36:11 +0800

    优化编辑页面

--- src/pages/Edit/template.vue
+++ src/pages/Edit/template.vue
@@ -2,11 +2,11 @@
   <div id="edit">
     <h1>编辑文章</h1>
     <h3>文章标题</h3>
-    <el-input maxlength="50" v-model="title" placeholder="请输入文章标题"></el-input>
-    <p class="msg">限50个字</p>
+    <el-input  v-model="title" maxlength="30" placeholder="请输入文章标题"></el-input>
+    <p class="msg">限制30个字</p>
     <h3>内容简介</h3>
     <el-input  maxlength="100" type="textarea" v-model="description" :autosize="{ minRows: 1, maxRows: 6}" placeholder="请输入内容简介"></el-input>
-    <p class="msg">限100个字</p>
+    <p class="msg">至少30个字</p>
     <h3>文章内容</h3>
     <p class="msg">至少200个字</p>
     <el-input type="textarea" v-model="content" :autosize="{ minRows: 4, maxRows: 30}" placeholder="请输入文章内容"></el-input>
